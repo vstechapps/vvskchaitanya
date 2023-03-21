215fe018d158f70bc4d24a983c94b7080b5ba768
vvskchaitanya <vvskchaitanya@gmail.com>
2023-03-21 06:47:08 +0000

    profile, timeline

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -9,5 +10,9 @@ export class MenuComponent {
 
   @Output()
   close = new EventEmitter();
+
+  constructor(public router:Router){
+
+  }
 
 }
