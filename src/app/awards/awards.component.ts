3a05c148fb54b0c9b3f9ff1a980d3b1e402d9b3c
vvskchaitanya <vvskchaitanya@gmail.com>
2023-03-26 11:07:45 +0000

    images

--- src/app/awards/awards.component.ts
+++ src/app/awards/awards.component.ts
@@ -8,20 +8,20 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class AwardsComponent {
   awards:{name:string,link:string,l?:any,t?:string}[]=[
-    {name:"TCS - Star of Month Award",link:"assets/files/tcs-star-of-the-month.pdf"},
-    {name:"Deloitte - Spot Award",link:"assets/files/deloitte-spot.pdf"},
-    {name:"Deloitte - Outstanding Award",link:"assets/files/deloitte-outstanding.pdf"},
-    {name:"Oracle - Java Associate",link:"assets/files/oracle-java-cert1.pdf"},
-    {name:"Oracle - Java Professional",link:"assets/files/oracle-java-cert2.pdf"},
-    {name:"Mongo DB - Certitifaction",link:"assets/files/udemy-mongodb.pdf"},
+    {name:"TCS - Star of Month Award",link:"assets/files/tcs-star-of-the-month.jpg"},
+    {name:"Deloitte - Spot Award",link:"assets/files/deloitte-spot.jpg"},
+    {name:"Deloitte - Outstanding Award",link:"assets/files/deloitte-outstanding.jpg"},
+    {name:"Oracle - Java Associate",link:"assets/files/oracle-java-cert1.jpg"},
+    {name:"Oracle - Java Professional",link:"assets/files/oracle-java-cert2.jpg"},
+    {name:"Mongo DB - Certitifaction",link:"assets/files/udemy-mongodb.jpg"},
     {name:"Microsoft Azure - Fudamentals",link:"assets/files/az-900.png"},
   ];
 
   constructor(private sanitizer:DomSanitizer){
     this.awards.forEach(a=>{
       a.l=this.sanitizer.bypassSecurityTrustResourceUrl(a.link);
-      if(a.link.includes(".pdf")){
-        a.t="application/pdf";
+      if(a.link.includes(".jpg")){
+        a.t="image/jpeg";
       }
       else if(a.link.includes(".png")){
         a.t="image/png";
