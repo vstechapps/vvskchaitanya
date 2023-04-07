b5bfedb4a40440d13acb2851111d27bd78a98921
vvskchaitanya <vvskchaitanya@gmail.com>
2023-04-07 19:24:49 +0000

    app store updates

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -8,13 +8,25 @@ import { Component } from '@angular/core';
 export class StoreComponent {
 
   apps=[{
+    name:"IAM",
+    about:"Identity and Access Management",
+    text:"Centralized Repository for User Management. Authenticate / Authorize users and maintain trust between users and enterprise resources. Manage User roles, permissions and business policies for complete organization."
+  },{
+    name:"ECM",
+    about:"Enterprise Content Management",
+    text:"Create Content, Review, Collaborate across Teams, Publish content to public. By default supports most of content types available in market. Use Content Modelling, Add required business rules, customize based on need "
+  },{
+    name:"BPM",
+    about:"Business Process Management",
+    text:"Easily create automated workflows required for your business process. Send out Alerts / Reminders to employees / managers / clients based on business rules. Eliminate manual supervision of business process"
+  },{
     name:"BETA",
     about:"Business Expense Tracking and Approval",
-    text:"Track and Supervise your business expenses spent by you / staff / employees / others, Upload the expense bills into this system and Eliminate the chances of spending unnecessary expenses"
+    text:"Track and Supervise your business expenses spent by you / staff / employees / other team members. Get the expense bills uploaded into system, Do Validate and Approve. Eliminate the chances of spending unnecessary expenses"
   },{
-    name:"My Networth",
-    about:"Track and Increase your Networth",
-    text:"Track your asset and liabilities. Calculate your current Networth according to the current market value of your assets. Explore options to increase Networth. Choose plans to achieve your financial goals in life"
+    name:"MNM",
+    about:"My Networth Management",
+    text:"Add and Track your asset and liabilities. Calculate your current Networth according to the current market value of your assets. Explore options to increase Networth. Choose plans to achieve your financial goals in life"
   }]
 
 
