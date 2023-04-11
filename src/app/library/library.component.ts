697c4511618767bf342d52fd28211bb2d8688dab
vvskchaitanya <vvskchaitanya@gmail.com>
2023-04-11 19:32:13 +0000

    added library

--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-library',
   templateUrl: './library.component.html',
   styleUrls: ['./library.component.less']
 })
-export class LibraryComponent {
+export class LibraryComponent implements OnInit{
+
+  pages: HTMLCollectionOf<Element>;
+
+  constructor(){
+    this.pages = document.getElementsByClassName('page');
+  }
+
+  ngOnInit(): void {
+    this.load();
+  }
+
+  load(){
+    
+    for(var i = 0; i < this.pages.length; i++)
+    {
+      let page:any = this.pages[i];
+      if (i % 2 === 0)
+        {
+          page.style.zIndex = (this.pages.length - i);
+        }
+    }
+    let a=this;
+    document.addEventListener('DOMContentLoaded', function(){
+      for(var i = 0; i < a.pages.length; i++)
+        {
+          let p:any = a.pages[i];
+          p.pageNum = i + 1;
+          p.onclick=function()
+            {
+              if (this.pageNum % 2 === 0)
+                {
+                  this.classList.remove('flipped');
+                  this.previousElementSibling.classList.remove('flipped');
+                }
+              else
+                {
+                  this.classList.add('flipped');
+                  this.nextElementSibling.classList.add('flipped');
+                }
+             }
+          }
+    })
+  }
 
 }
