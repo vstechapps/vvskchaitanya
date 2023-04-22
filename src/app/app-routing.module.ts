d3732c361d55f22ae54551fbcad7d20c4bab3c95
vvskchaitanya <vvskchaitanya@gmail.com>
2023-04-22 20:53:33 +0000

    ecm, bpm, mdm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,10 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path:"",component:HomeComponent},
-  {path:"iam",loadChildren: () => import('./iam/iam.module').then(m => m.IamModule)}
+  {path:"iam",loadChildren: () => import('./iam/iam.module').then(m => m.IamModule)},
+  {path:"ecm",loadChildren: () => import('./ecm/ecm.module').then(m => m.EcmModule)},
+  {path:"bpm",loadChildren: () => import('./bpm/bpm.module').then(m => m.BpmModule)},
+  {path:"mdm",loadChildren: () => import('./mdm/mdm.module').then(m => m.MdmModule)}
 ];
 
 @NgModule({
